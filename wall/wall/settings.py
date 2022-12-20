e8df584ceccce34248c56ee67d912e72df7b55e5
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-20 16:01:02 -0300

    chore: setup mysql database

--- wall/wall/settings.py
+++ wall/wall/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@g47oj$ly8prq1k&60qcf860s3d2oby+fv1w+uvfb!n5+#mbtp"
+# SECRET_KEY = "django-insecure-@g47oj$ly8prq1k&60qcf860s3d2oby+fv1w+uvfb!n5+#mbtp"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +80,11 @@ WSGI_APPLICATION = "wall.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
     }
 }
 
